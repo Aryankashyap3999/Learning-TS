63c8853762cf28a2af0366b4d32dad31ac2282a8
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-10-02 22:46:15 +0530

    Added L5

---
+++ L5.ts
@@ -0,0 +1,15 @@
+// Date in TS
+
+const dob = new Date(1998, 3, 11);
+console.log(dob);
+
+/*
+    any type -> free ticket to get rid of type checking
+
+    If want a variable has type any use : any or just don't assign value to it.
+*/
+
+let x; // here x has type any
+x = '10';
+x = 10;
+

9a761efa5096f39670dd47e74cd20d2b5665c697
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-10-02 22:51:20 +0530

    Lesson6

---
+++ L6.ts
@@ -0,0 +1,14 @@
+function abc() : void { // explicitly defines void as return type
+    
+}
+
+// want to  return more types.
+
+
+// function acb() : string | number {
+//     return 1;
+// }
+
+// function acb() : string | number {
+//     return '1';
+// }
